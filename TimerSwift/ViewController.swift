1a3eb2a9dd5c2817663e0c12b58903cae8640c71
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-11-29 14:59:54 +0800

    Initial Commit

---
+++ TimerSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerSwift
+//
+//  Created by kenneth.ng@lifehikes.com on 29/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
