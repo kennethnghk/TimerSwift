348747a47d92336b5443d7f4c741ee30352f4dad
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-11-29 16:34:51 +0800

    Initial commit

--- TimerSwift/ViewController.swift
+++ TimerSwift/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let levels : [String : Int] = [
+        "Small": 30,
+        "Medium": 60,
+        "Large": 90
+    ]
+    
+    var selectedLevel : String = "Medium"
+    var totalSeconds : Int = 60
+    var passedSeconds : Int = 0
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("button pressed")
+        
+        selectedLevel = sender.titleLabel!.text!
+        totalSeconds = levels[selectedLevel]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer()
+    {
+        if passedSeconds < totalSeconds {
+            print("\(passedSeconds) of \(totalSeconds) seconds.")
+            
+            let percent = Float(passedSeconds) / Float(totalSeconds)
+            print("progress \(percent) / \(totalSeconds)" )
+            progressView.progress = percent
+            
+            passedSeconds += 1
+            
+        } else {
+            progressView.progress = 0.0
+            timer.invalidate()
+        }
+        
+        
+    }
+    
 }
 
